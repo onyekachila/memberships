d9bb741c0386f973de8eee0e209b35af69697649
onyekachila <onyekachila@gmail.com>
2019-08-21 14:07:17 +0100

    Added Admin Menu
    Added admin menu and js bundling in _layout and referenced in bundle config

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Memberships
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                      "~/Scripts/AdminMenu.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
